c4b900ecc3eec6ef558141a09b73e5d7e9e0e8b4
starttaskgithub <derangulaanudeepthi@gmail.com>
2022-02-27 19:01:48 +0530

    first commit

---
+++ all-files/src/App.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+// import Navbar from '../src/components/Navbar';
+// import './App.css';
+// import Home from '../src/components/Pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import Services from '../src/components/Pages/Products';
+// import Products from '../src/components/Pages/Services';
+// import SignUp from '../src/components/Pages/Signup';
+
+function App() {
+  return (
+    <div>
+  hiii
+      <Router>
+        
+        <switch>
+          <Route path='/' exact />
+       
+        </switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ all-files/src/components/Pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../App.css';
+
+// import HeroSection from '../HeroSection';
+
+
+function Home() {
+  return (
+    <div>
+      
+      
+      
+    </div>
+  );
+}
+
+export default Home;
